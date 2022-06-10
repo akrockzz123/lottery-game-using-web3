df79c85eb56b4930926ad12691347fa481ac4cb8
Aniket Kumar <aniketkumar1601@gmail.com>
2022-06-10 14:00:58 +0530

    added pickwinner

--- src/App.js
+++ src/App.js
@@ -15,7 +15,50 @@ const App = () => {
 
   const [players,setPlayers] = useState([])
 
-  const [balance,setBalance] = useState(0)
+  const [balance,setBalance] = useState('')
+
+  const [value,setValue] = useState('')
+
+  const [message,setMessage] = useState('')
+
+  console.log(value)
+
+  const submitHandler = async (e) => {
+
+    e.preventDefault()
+
+    // get list of all accounts
+
+    const accounts = await web3.eth.getAccounts();
+
+    console.log(typeof(value))
+    setMessage('waiting for transaction to over')
+
+
+    await lottery.methods.enter().send({   // entering to lottery
+      from:  accounts[0],
+      value: web3.utils.toWei(value,'ether')
+    });
+
+    setMessage('You have entered the lottery')
+
+
+
+  };
+
+
+  const pickWin = async() => {
+
+    const accounts = await web3.eth.getAccounts(); // get all accounts
+
+    setMessage('Hold on a second waiting')
+    const winner = await lottery.methods.pickWinner().send({
+      from: accounts[0]
+    });
+
+    setMessage('winner has been picked',winner)
+
+  }
 
   useEffect(() => {
 
@@ -38,16 +81,37 @@ const App = () => {
     fun();
 
     
-  },[])
+  },)
 
   return (
     <div>
       <h2>LOTTERY CONTRACT</h2>
       <p>
         This contract is managed by {mngr} manager
-        This account holds {web3.utils.toWei(balance, 'ether')} Balance and there are {players.length} people to win the lottery
+        This account holds {web3.utils.fromWei(balance, 'ether')} Balance and there are {players.length} people to win the lottery
       </p>
-     
+      <hr/>
+
+      <form onSubmit = {submitHandler}>
+        <h4>want to try your luck</h4>
+
+        <div>
+          <label>Amount of ether to enter</label>
+
+          <input  value = {value} onChange = {(event) => setValue(event.target.value)}/>
+        </div>
+
+        <button>Enter</button>
+
+        <hr/>
+        <hr/>
+
+        <h4>Ready to pick a winner</h4>
+
+        <buuton onClick = {pickWin}>Pick a winner</buuton>
+
+        <h1>{message}</h1>
+      </form>
     </div>
   )
 }
--- src/lottery.js
+++ src/lottery.js
@@ -1,6 +1,6 @@
 import web3 from './web3';
 
-const address = '0xCf73116aF3CbA0fC8c53c7Ded9AcCF55B61E9EBa' // address on which contract was deployed
+const address = '0xA4364a76a4a3D02bf6E83186063457f813F3692E' // address on which contract was deployed
 
 const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]
 
