63f731a4790078e513016f885c346bbb1f606d4d
Aniket Kumar <aniketkumar1601@gmail.com>
2022-06-10 10:09:24 +0530

    displaying current balance and no of people in lottery

--- src/App.js
+++ src/App.js
@@ -1,16 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import web3 from './web3';
 
+import lottery from './lottery'
+
+import { useEffect } from 'react';
+import { render } from '@testing-library/react';
+
+
+
 const App = () => {
 
-  console.log(web3.version)
+  const [mngr,setMngr] = useState('')
 
-  web3.eth.getAccounts().then(console.log) // get all acounts that metamask provided by our installed web3
+  const [players,setPlayers] = useState([])
+
+  const [balance,setBalance] = useState(0)
+
+  useEffect(() => {
+
+    const fun = async () => {
+
+      const manager = await lottery.methods.manager().call();
+
+      const players = await lottery.methods.getPlayers().call();
+
+      const balance = await web3.eth.getBalance(lottery.options.address)
+
+      setMngr(manager)
+
+      setPlayers(players)
+
+      setBalance(balance)
+
+    }
+
+    fun();
+
+    
+  },[])
 
   return (
-
-    <div>App</div>
+    <div>
+      <h2>LOTTERY CONTRACT</h2>
+      <p>
+        This contract is managed by {mngr} manager
+        This account holds {web3.utils.toWei(balance, 'ether')} Balance and there are {players.length} people to win the lottery
+      </p>
+     
+    </div>
   )
 }
 
